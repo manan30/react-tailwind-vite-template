3acd454f947c403b48a0171fe5f5b9a96acb0cbe
Manan Joshi <mananjoshi1995@gmail.com>
2021-06-28 14:27:39 -0500

    Setting up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.export = {
+  purge: ['./index.html', './src/**/*.tsx'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
